4c20595b75bfe3b3ed06b21fc754b7c7c6e7775d
cederman <marten@mcederman.com>
2013-03-05 12:57:02 +0100

    The "proceed" button now cycles trough strings onClick

--- src/com/medieteknik/dyslexia/svp/MainActivity.java
+++ src/com/medieteknik/dyslexia/svp/MainActivity.java
@@ -8,25 +8,26 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+  
+    int counter;
+    String s = "Iphone swag fanny pack, try-hard master cleanse pop-up elit deserunt vinyl. Odio mustache hoodie YOLO fap. Vegan godard labore tempor. Farm-to-table aute craft beer, YOLO bicycle rights artisan semiotics. Cosby sweater readymade eiusmod consectetur fap stumptown. Cliche keytar accusamus blue bottle, wayfarers locavore selfies elit aliqua chillwave lo-fi helvetica enim. Irony tofu blog, fap pickled pariatur odio wolf cliche vice sint pitchfork eiusmod cosby sweater polaroid.";
+    final String[] words = s.split("\\s+");
+    Button b = (Button) findViewById(R.id.next_button);
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    
-    String s = "This is a sample sentence.";
-    final String[] words = s.split("\\s+");
+        final TextView tx = (TextView) findViewById(R.id.text_box);
+        counter = 0;
+        tx.setText(words[counter]);
         
-    Button b = (Button) findViewById(R.id.next_button);
-
     b.setOnClickListener(new OnClickListener() {
        @Override
        public void onClick(View v) {
-           TextView tx = (TextView) findViewById(R.id.text_box);
-           StringBuilder builder = new StringBuilder();
-           for (String s : words){
-             builder.append(s+" ");
-           tx.setText(builder.toString());
+           counter++;
+           tx.setText(words[counter]);
        }
-    }});
+    });
     
 }}
