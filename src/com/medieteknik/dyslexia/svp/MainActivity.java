e98e0dfe70294fd825e1f33328671267974432b3
cederman <marten@mcederman.com>
2013-03-05 13:08:47 +0100

    Avoid application crash when there is no text to display anymore

--- src/com/medieteknik/dyslexia/svp/MainActivity.java
+++ src/com/medieteknik/dyslexia/svp/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends Activity {
     int counter;
     String s = "Iphone swag fanny pack, try-hard master cleanse pop-up elit deserunt vinyl. Odio mustache hoodie YOLO fap. Vegan godard labore tempor. Farm-to-table aute craft beer, YOLO bicycle rights artisan semiotics. Cosby sweater readymade eiusmod consectetur fap stumptown. Cliche keytar accusamus blue bottle, wayfarers locavore selfies elit aliqua chillwave lo-fi helvetica enim. Irony tofu blog, fap pickled pariatur odio wolf cliche vice sint pitchfork eiusmod cosby sweater polaroid.";
     final String[] words = s.split("\\s+");
-    Button b = (Button) findViewById(R.id.next_button);
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +21,18 @@ public class MainActivity extends Activity {
         counter = 0;
         tx.setText(words[counter]);
         
+    Button b = (Button) findViewById(R.id.next_button);
+
     b.setOnClickListener(new OnClickListener() {
        @Override
        public void onClick(View v) {
-           counter++;
-           tx.setText(words[counter]);
+    	   try{
+    		   counter++;
+               tx.setText(words[counter]);   
+    	   }
+    	   catch( ArrayIndexOutOfBoundsException e){
+    		   tx.setText("End of text =(");
+    	   }
        }
     });
     
