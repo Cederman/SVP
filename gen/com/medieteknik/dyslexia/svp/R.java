1a4bdffd012939890423fe7eaa766af221253ef5
Anton Warnhag <anton.warnhag@gmail.com>
2013-03-06 16:08:23 +0100

    Connected activities via buttons

--- gen/com/medieteknik/dyslexia/svp/R.java
+++ gen/com/medieteknik/dyslexia/svp/R.java
@@ -24,8 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int next_button=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int nav_button=0x7f080001;
+        public static final int play_button=0x7f080003;
+        public static final int textView1=0x7f080002;
         public static final int text_box=0x7f080000;
     }
     public static final class layout {
@@ -41,6 +43,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int button_nav_go=0x7f050008;
+        public static final int button_play_go=0x7f050009;
         public static final int hello_world=0x7f050006;
         public static final int menu_settings=0x7f050001;
         public static final int next=0x7f050004;
